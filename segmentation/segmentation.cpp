19d677626379b04897c127fd54490fc589fb7261
Kai Klindworth <github@kk-surf.de>
2014-10-20 20:29:04 +0200

    moved cr segmentation algorithm into seperate file

--- segmentation/segmentation.cpp
+++ segmentation/segmentation.cpp
@@ -40,9 +40,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <stdexcept>
 #include <fstream>
 
-#include "contourRelaxation/FeatureType.h"
-#include "contourRelaxation/ContourRelaxation.h"
-#include "contourRelaxation/InitializationFunctions.h"
+#include "segmentation_cr.h"
 
 int cachedSegmentation(StereoSingleTask& task, cv::Mat_<int>& labels, std::shared_ptr<segmentation_algorithm>& algorithm)
 {
@@ -347,49 +345,6 @@ std::string slic_segmentation::cacheName() const
 	return stream.str();
 }*/
 
-
-int crslic_segmentation::operator()(const cv::Mat& image, cv::Mat_<int>& labels)
-{
-	float directCliqueCost = 0.3;
-	unsigned int const iterations = 3;
-	double const diagonalCliqueCost = directCliqueCost / sqrt(2);
-
-	bool isColorImage = (image.channels() == 3);
-	std::vector<FeatureType> features;
-	if (isColorImage)
-		features.push_back(Color);
-	else
-		features.push_back(Grayvalue);
-
-	features.push_back(Compactness);
-
-	ContourRelaxation<int> crslic_obj(features);
-	cv::Mat labels_temp = createBlockInitialization<int>(image.size(), settings.superpixel_size, settings.superpixel_size);
-
-	crslic_obj.setCompactnessData(settings.superpixel_compactness);
-
-	if (isColorImage)
-	{
-		cv::Mat imageYCrCb;
-		cv::cvtColor(image, imageYCrCb, CV_BGR2YCrCb);
-		std::vector<cv::Mat> imageYCrCbChannels;
-		cv::split(imageYCrCb, imageYCrCbChannels);
-
-		crslic_obj.setColorData(imageYCrCbChannels[0], imageYCrCbChannels[1], imageYCrCbChannels[2]);
-	}
-	else
-		crslic_obj.setGrayvalueData(image.clone());
-
-	crslic_obj.relax(labels_temp, directCliqueCost, diagonalCliqueCost, iterations, labels);
-	return 1+*(std::max_element(labels.begin(), labels.end()));
-}
-
-std::string crslic_segmentation::cacheName() const
-{
-	std::stringstream stream;
-	stream << "crsuperpixel_" << settings.superpixel_size;
-	return stream.str();
-}
 
 /*int meanshift_segmentation::operator()(const cv::Mat& image, cv::Mat_<int>& labels) {
 	return mean_shift_segmentation(image, labels, settings.spatial_var, settings.color_var, 20);
---
+++ segmentation/segmentation_cr.cpp
@@ -0,0 +1,49 @@
+#include "segmentation_cr.h"
+
+#include "contourRelaxation/FeatureType.h"
+#include "contourRelaxation/ContourRelaxation.h"
+#include "contourRelaxation/InitializationFunctions.h"
+
+int crslic_segmentation::operator()(const cv::Mat& image, cv::Mat_<int>& labels)
+{
+	float directCliqueCost = 0.3;
+	unsigned int const iterations = 3;
+	double const diagonalCliqueCost = directCliqueCost / sqrt(2);
+
+	bool isColorImage = (image.channels() == 3);
+	std::vector<FeatureType> features;
+	if (isColorImage)
+		features.push_back(Color);
+	else
+		features.push_back(Grayvalue);
+
+	features.push_back(Compactness);
+
+	ContourRelaxation<int> crslic_obj(features);
+	cv::Mat labels_temp = createBlockInitialization<int>(image.size(), settings.superpixel_size, settings.superpixel_size);
+
+	crslic_obj.setCompactnessData(settings.superpixel_compactness);
+
+	if (isColorImage)
+	{
+		cv::Mat imageYCrCb;
+		cv::cvtColor(image, imageYCrCb, CV_BGR2YCrCb);
+		std::vector<cv::Mat> imageYCrCbChannels;
+		cv::split(imageYCrCb, imageYCrCbChannels);
+
+		crslic_obj.setColorData(imageYCrCbChannels[0], imageYCrCbChannels[1], imageYCrCbChannels[2]);
+	}
+	else
+		crslic_obj.setGrayvalueData(image.clone());
+
+	crslic_obj.relax(labels_temp, directCliqueCost, diagonalCliqueCost, iterations, labels);
+	return 1+*(std::max_element(labels.begin(), labels.end()));
+}
+
+std::string crslic_segmentation::cacheName() const
+{
+	std::stringstream stream;
+	stream << "crsuperpixel_" << settings.superpixel_size;
+	return stream.str();
+}
+
