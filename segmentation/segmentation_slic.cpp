029e66d67c5ced141f781c7ec508e2d591c1b871
Kai Klindworth <github@kk-surf.de>
2014-10-21 10:32:36 +0200

    make use of override

--- segmentation/segmentation_slic.cpp
+++ segmentation/segmentation_slic.cpp
@@ -2,7 +2,7 @@
 
 #include "SLIC_CV/slic_adaptor.h"
 
-int slic_segmentation::operator()(const cv::Mat& image, cv::Mat& labels) {
+int slic_segmentation::operator()(const cv::Mat& image, cv::Mat_<int>& labels) {
 	return slicSuperpixels(image, labels, settings.superpixel_size, settings.superpixel_compactness);
 }
 
