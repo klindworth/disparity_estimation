39c87b09ca8d0491f860ccbfeba73519ae463e1d
Kai Klindworth <github@kk-surf.de>
2014-11-25 11:50:42 +0100

    additional tests

--- segmentation/tests.cpp
+++ segmentation/tests.cpp
@@ -1,6 +1,13 @@
 #include "region_descriptor.h"
 #include "region_descriptor_algorithms.h"
 #include "segmentation_refinement.h"
+#include "intervals.h"
+#include "intervals_algorithms.h"
+#include <iostream>
+#include <assert.h>
+#include <vector>
+#include <algorithm>
+#include <iterator>
 
 #include <gtest/gtest.h>
 
@@ -69,4 +76,237 @@ TEST(SplitRegionTest, Basic2)
 	EXPECT_EQ(ret5, 4);
 	EXPECT_EQ(res5.size(), 4);
 }
+
+template<typename Iterator>
+bool mismatch_output(Iterator it1begin, Iterator it1end, Iterator it2begin)
+{
+	auto it = std::mismatch(it1begin, it1end, it2begin);
+	if(it.first != it1end)
+	{
+		std::cout << "first mismatch: " << *(it.first) << " vs " << *(it.second) << std::endl;
+		return false;
+	}
+	return true;
+}
+
+//ported
+bool simple_interval_test()
+{
+	std::vector<RegionInterval> base {RegionInterval(0, 2, 6), RegionInterval(0, 10, 19), RegionInterval(0,22,29), RegionInterval(0,30,36), RegionInterval(1,2,6), RegionInterval(2,4,9)};
+	std::vector<RegionInterval> match {RegionInterval(0, 3,5), RegionInterval(0,9,13), RegionInterval(0,15,21), RegionInterval(0,23,26), RegionInterval(0,36, 41), RegionInterval(2,5,7)};
+	std::vector<RegionInterval> difference;
+	//std::vector<RegionInterval> intersection;
+
+	std::vector<RegionInterval> difference_desired {RegionInterval(0,2,3), RegionInterval(0,5,6), RegionInterval(0, 13, 15), RegionInterval(0,22,23), RegionInterval(0,26,29), RegionInterval(0,30,36), RegionInterval(1,2,6), RegionInterval(2,4,5),RegionInterval(2, 7, 9)};
+	//std::vector<RegionInterval> intersection_desired {RegionInterval(0,3,5), RegionInterval(0,10,13), RegionInterval(0,15,19), RegionInterval(0,23,26), RegionInterval(2,5,7)};
+
+	//intervals::intersection(base.begin(), base.end(), match.begin(), match.end(), 0, std::back_inserter(intersection), std::back_inserter(difference));
+
+	//std::cout << std::endl << "--intersections--" << std::endl;
+	//std::copy(intersection.begin(), intersection.end(), std::ostream_iterator<RegionInterval>(std::cout, " "));
+
+	/*std::cout << std::endl << "--difference--" << std::endl;
+	std::copy(difference.begin(), difference.end(), std::ostream_iterator<RegionInterval>(std::cout, " "));
+	std::cout << std::endl;
+
+	std::cout << "difference" << std::endl;*/
+	if(!mismatch_output(difference.begin(), difference.end(), difference_desired.begin()))
+		return false;
+
+	/*std::cout << "intersection" << std::endl;
+	if(!mismatch_output(intersection.begin(), intersection.end(), intersection_desired.begin()))
+		return false;*/
+
+	return true;
+}
+
+//ported
+bool simple_interval_test2()
+{
+	std::vector<RegionInterval> base {RegionInterval(0,1,2), RegionInterval(0, 2, 6), RegionInterval(0, 10, 19), RegionInterval(0,22,29), RegionInterval(0,30,36), RegionInterval(1,2,6), RegionInterval(2,4,9), RegionInterval(5,1,9), RegionInterval(6,2,6)};
+	std::vector<RegionInterval> match {RegionInterval(0,1,2), RegionInterval(0, 3,5), RegionInterval(0,9,13), RegionInterval(0,15,21), RegionInterval(0,23,26), RegionInterval(0,36, 41), RegionInterval(2,5,7), RegionInterval(3,5,9), RegionInterval(5,3,6), RegionInterval(6,2,6)};
+	std::vector<RegionInterval> difference;
+	//std::vector<RegionInterval> intersection;
+
+	std::vector<RegionInterval> difference_desired {RegionInterval(0,2,3), RegionInterval(0,5,6), RegionInterval(0, 13, 15), RegionInterval(0,22,23), RegionInterval(0,26,29), RegionInterval(0,30,36), RegionInterval(1,2,6), RegionInterval(2,4,5),RegionInterval(2, 7, 9), RegionInterval(5,1,3), RegionInterval(5,6,9)};
+	/*std::vector<RegionInterval> intersection_desired {RegionInterval(0,1,2), RegionInterval(0,3,5), RegionInterval(0,10,13), RegionInterval(0,15,19), RegionInterval(0,23,26), RegionInterval(2,5,7), RegionInterval(5,3,6), RegionInterval(6,2,6)};
+
+	intervals::intersection(base.begin(), base.end(), match.begin(), match.end(), 0, std::back_inserter(intersection), std::back_inserter(difference));
+
+	std::cout << std::endl << "--intersections--" << std::endl;
+	std::copy(intersection.begin(), intersection.end(), std::ostream_iterator<RegionInterval>(std::cout, " "));*/
+
+	/*std::cout << std::endl << "--difference--" << std::endl;
+	std::copy(difference.begin(), difference.end(), std::ostream_iterator<RegionInterval>(std::cout, " "));
+	std::cout << std::endl;*/
+
+	//std::cout << "difference" << std::endl;
+	if(!mismatch_output(difference.begin(), difference.end(), difference_desired.begin()))
+		return false;
+
+	/*std::cout << "intersection" << std::endl;
+	if(!mismatch_output(intersection.begin(), intersection.end(), intersection_desired.begin()))
+		return false;*/
+
+	return true;
+}
+
+//ported
+bool simple_interval_test3()
+{
+	std::vector<RegionInterval> base  {RegionInterval(0, 319, 329), RegionInterval(1, 318, 329), RegionInterval(2, 318, 329), RegionInterval(4, 318,329), RegionInterval(5, 318,329), RegionInterval(11, 317,318), RegionInterval(12, 317,318)};
+	std::vector<RegionInterval> match {RegionInterval(0, 327, 328), RegionInterval(1, 326, 328), RegionInterval(2, 322, 328), RegionInterval(4, 321,322), RegionInterval(4, 323,328)};
+	std::vector<RegionInterval> difference;
+	//std::vector<RegionInterval> intersection;
+
+	std::vector<RegionInterval> difference_desired {RegionInterval(0,319,327), RegionInterval(0,328,329), RegionInterval(1, 318, 326), RegionInterval(1,328,329), RegionInterval(2,318,322), RegionInterval(2,328,329), RegionInterval(4, 318,321), RegionInterval(4, 322,323), RegionInterval(4, 328,329), RegionInterval(5, 318,329), RegionInterval(11, 317,318), RegionInterval(12, 317,318)};
+	std::vector<RegionInterval> intersection_desired {RegionInterval(0, 327, 328), RegionInterval(1, 326, 328), RegionInterval(2, 322, 328), RegionInterval(4, 321,322), RegionInterval(4, 323,328)};
+
+	/*intervals::intersection(base.begin(), base.end(), match.begin(), match.end(), 0, std::back_inserter(intersection), std::back_inserter(difference));
+
+	std::cout << std::endl << "--intersections--" << std::endl;
+	std::copy(intersection.begin(), intersection.end(), std::ostream_iterator<RegionInterval>(std::cout, " "));*/
+
+	/*std::cout << std::endl << "--difference--" << std::endl;
+	std::copy(difference.begin(), difference.end(), std::ostream_iterator<RegionInterval>(std::cout, " "));
+	std::cout << std::endl;*/
+
+	//std::cout << "difference" << std::endl;
+	if(!mismatch_output(difference.begin(), difference.end(), difference_desired.begin()))
+		return false;
+
+	/*std::cout << "intersection" << std::endl;
+	if(!mismatch_output(intersection.begin(), intersection.end(), intersection_desired.begin()))
+		return false;
+
+	std::vector<RegionInterval> diff_test;
+	intervals::difference(base.begin(), base.end(), match.begin(), match.end(), 0, std::back_inserter(diff_test), true);
+
+
+	std::cout << "difference2" << std::endl;
+	if(!mismatch_output(diff_test.begin(), diff_test.end(), difference_desired.begin()))
+		return false;*/
+
+	return true;
+}
+
+//ported
+bool simple_interval_test_disparity()
+{
+	std::vector<RegionInterval> base {RegionInterval(0, 2, 6), RegionInterval(0, 10, 19), RegionInterval(0,22,29), RegionInterval(0,30,36), RegionInterval(1,2,6), RegionInterval(2,4,9)};
+	std::vector<RegionInterval> match {RegionInterval(0, 3,5), RegionInterval(0,9,13), RegionInterval(0,15,21), RegionInterval(0,23,26), RegionInterval(0,36, 41), RegionInterval(2,5,7)};
+	std::vector<RegionInterval> difference;
+	//std::vector<RegionInterval> intersection;
+
+	std::vector<RegionInterval> difference_desired {RegionInterval(0,2,4), RegionInterval(0, 14, 16), RegionInterval(0,22,24), RegionInterval(0,27,29), RegionInterval(0,30,36), RegionInterval(1,2,6), RegionInterval(2,4,6),RegionInterval(2, 8, 9)};
+	/*std::vector<RegionInterval> intersection_desired {RegionInterval(0,4,6), RegionInterval(0,10,14), RegionInterval(0,16,19), RegionInterval(0,24,27), RegionInterval(2,6,8)};
+
+	intervals::intersection(base.begin(), base.end(), match.begin(), match.end(), -1, std::back_inserter(intersection), std::back_inserter(difference), true);
+
+	std::cout << std::endl << "--intersections--" << std::endl;
+	std::copy(intersection.begin(), intersection.end(), std::ostream_iterator<RegionInterval>(std::cout, " "));*/
+
+	/*std::cout << std::endl << "--difference--" << std::endl;
+	std::copy(difference.begin(), difference.end(), std::ostream_iterator<RegionInterval>(std::cout, " "));
+	std::cout << std::endl;*/
+
+	//std::cout << "difference" << std::endl;
+	if(!mismatch_output(difference.begin(), difference.end(), difference_desired.begin()))
+		return false;
+
+	/*std::cout << "intersection" << std::endl;
+	if(!mismatch_output(intersection.begin(), intersection.end(), intersection_desired.begin()))
+		return false;*/
+
+	return true;
+}
+
+//ported
+TEST(Interval, ValueRegion)
+{
+	cv::Mat mat(1, 20, CV_8UC1, cv::Scalar(0));
+	for(int i = 3; i <= 5; ++i)
+		mat.at<unsigned char>(0,i) = 2;
+	for(int i = 9; i <= 14; ++i)
+		mat.at<unsigned char>(0,i) = 5;
+
+	std::vector<ValueRegionInterval<unsigned char>> desired = {ValueRegionInterval<unsigned char>(0, 0, 3, 0), ValueRegionInterval<unsigned char>(0, 3, 6, 2), ValueRegionInterval<unsigned char>(0, 6, 9, 0), ValueRegionInterval<unsigned char>(0, 9, 15, 5), ValueRegionInterval<unsigned char>(0, 15, 20, 0)};
+
+	std::vector<ValueRegionInterval<unsigned char>> calc;
+
+	cv::Mat_<unsigned char> mat1 = mat;
+
+	intervals::convertFromMatToValue(mat1, std::back_inserter(calc));
+
+	/*std::cout << "---- value intervals ----" << std::endl;
+	std::copy(calc.begin(), calc.end(), std::ostream_iterator<RegionInterval>(std::cout, ", "));
+	std::cout << std::endl;*/
+
+	EXPECT_TRUE(mismatch_output(calc.begin(), calc.end(), desired.begin()));
+}
+
+//ported
+TEST(Interval, ThresholdConvert)
+{
+	cv::Mat_<unsigned char> test(1, 20, 15);
+
+	for(int i = 0; i < 5; ++i)
+	{
+		test.at<unsigned char>(i+5) = 5;
+		test.at<unsigned char>(i+12) = 5;
+	}
+	std::vector<RegionInterval> result;
+	intervals::convertMinimaRanges(test, std::back_inserter(result), (unsigned char)10);
+
+	std::vector<RegionInterval> expected {RegionInterval(0, 5, 10), RegionInterval(0,12,17)};
+
+	EXPECT_TRUE(mismatch_output(result.begin(), result.end(), expected.begin()));
+}
+
+//ported
+TEST(Interval, Intersecting)
+{
+	RegionInterval test1(0,1,4);
+	RegionInterval test2(0,2,5);
+	RegionInterval test3(0,4,9);
+	EXPECT_TRUE(test1.intersecting(test2));
+	EXPECT_FALSE(test1.intersecting(test3));
+	EXPECT_TRUE(test2.intersecting(test3));
+
+	EXPECT_TRUE(test1.intersectingOrdered(test2));
+	EXPECT_FALSE(test1.intersectingOrdered(test3));
+	EXPECT_TRUE(test2.intersectingOrdered(test3));
+
+	EXPECT_TRUE(test2.intersecting(test1));
+	EXPECT_FALSE(test3.intersecting(test1));
+	EXPECT_TRUE(test3.intersecting(test2));
+}
+
+/*void value_intersecting_test()
+{
+	std::cout << "---- value intersecting test ----" << std::endl;
+	std::vector<ValueRegionInterval<short> > vec {ValueRegionInterval<short>(0,1,5,1), ValueRegionInterval<short>(0,3,8,2), ValueRegionInterval<short>(0,5,8,2)};
+	std::vector<ValueRegionInterval<short> > desired {ValueRegionInterval<short>(0,1,2,1), ValueRegionInterval<short>(0,3,8,2)};
+
+	auto value_compare = [](const ValueRegionInterval<short>& i1, const ValueRegionInterval<short>& i2) {
+		return i1.value > i2.value;
+	};
+
+	intervals::cleanup<ValueRegionInterval<short>>(vec.begin(), vec.end(), value_compare);
+	vec.erase(std::remove_if(vec.begin(), vec.end(), [](const ValueRegionInterval<short>& interval) {return interval.length() <= 0;}), vec.end());
+
+	mismatch_output(vec.begin(), vec.end(), desired.begin());
+
+
+}*/
+
+TEST(Interval, Basic)
+{
+	EXPECT_TRUE(simple_interval_test());
+	EXPECT_TRUE(simple_interval_test2());
+	EXPECT_TRUE(simple_interval_test3());
+	EXPECT_TRUE(simple_interval_test_disparity());
+	//EXPECT_TRUE(value_interval_test());
+}
+
 
