5a51237699ab32009b3f2b78efc3d32d80e64762
Kai Klindworth <github@kk-surf.de>
2014-12-22 16:10:15 +0100

    ported to qt5

--- main.cpp
+++ main.cpp
@@ -138,8 +138,8 @@ int main(int argc, char *argv[])
 
 	//RGB tasks
 	//StereoTask testset("tasks/im2rgb");
-	folder_testset testset("tasks/kitti-training_small");
-	//FolderTestSet testset("tasks/kitti-training_debug");
+	//folder_testset testset("tasks/kitti-training_small");
+	folder_testset testset("tasks/kitti-training_debug");
 	//StereoTask testset("tasks/kit3"); //5, 3 neuer problemfall?
 
 	//simulated (hard) tasks
