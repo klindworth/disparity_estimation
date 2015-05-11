c49d01e3cd7df6852fc9c2e8a92dc3dbd0d23b5b
Kai Klindworth <kai@localhost.localdomain>
2015-05-11 21:46:15 +0200

    extended disparity_range_tests

--- disparity_range_tests.cpp
+++ disparity_range_tests.cpp
@@ -54,3 +54,37 @@ TEST(DispRange, SubSpaceNegBorder)
 	EXPECT_EQ(15, test2.index(0));
 	EXPECT_EQ( 0, test2.index(-15));
 }
+
+TEST(DispRange, ImRestrictPos)
+{
+	disparity_range test(0,128);
+	int width = 400;
+	for(int i = 0; i < width; ++i)
+	{
+		disparity_range rtest = test.restrict_to_image(i, width);
+		ASSERT_EQ(test.offset(), rtest.offset());
+		ASSERT_EQ(0, rtest.start());
+		if(i < width - test.end())
+			ASSERT_EQ(rtest.size(), test.size());
+		else
+			ASSERT_EQ(width - 1, rtest.end() + i);
+		ASSERT_TRUE(rtest.end() + i < width);
+	}
+}
+
+TEST(DispRange, ImRestrictNeg)
+{
+	disparity_range test(-128,0);
+	int width = 400;
+	for(int i = 0; i < width; ++i)
+	{
+		disparity_range rtest = test.restrict_to_image(i, width);
+		ASSERT_EQ(test.offset(), rtest.offset());
+		ASSERT_EQ(0, rtest.end());
+		if(i + test.start() < 0)
+			ASSERT_EQ(0, rtest.start()+i);
+		else
+			ASSERT_EQ(rtest.size(), test.size());
+		ASSERT_TRUE(rtest.start() + i >= 0);
+	}
+}
